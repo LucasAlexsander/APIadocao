17f30865b0516146bb963f249ca56c6f0a931a7c
Lucas Alexsander Barbosa Cruz <lucas10barbosa_@hotmail.com>
2023-06-28 10:16:09 -0300

    Adicionando mais tipos de headers

--- src/app.js
+++ src/app.js
@@ -11,8 +11,14 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "apiadocao.onrender.com");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
   );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
