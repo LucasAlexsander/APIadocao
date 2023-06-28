82c00d6ccf308a76f54cb7841326b435edef7fbe
Lucas Alexsander Barbosa Cruz <lucas10barbosa_@hotmail.com>
2023-06-28 12:30:41 -0300

    alterando o method

--- src/app.js
+++ src/app.js
@@ -20,8 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "apiadocao.onrender.com");
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   app.use(cors());
   next();
