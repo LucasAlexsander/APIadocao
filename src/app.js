3d6102a6b2bd14d88cb6a9d3de27f1366da07720
Lucas Alexsander Barbosa Cruz <lucas10barbosa_@hotmail.com>
2023-06-28 09:12:32 -0300

    First Commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+   res.setHeader("Access-Control-Allow-Origin", "apieng.onrender.com");
+   res.header(
+     "Access-Control-Allow-Headers",
+     "Origin, X-Requested-With, Content-Type, Accept"
+   );
+   next();
+ })
+
+//rotas importadas de router
+app.use('/animais', require('./Routes/animalRoutes'))
+app.use('/admin', require('./Routes/adminRoutes'))
+app.use('/adotantes', require('./Routes/adotanteRoutes'))
+app.use('/resgates', require('./Routes/resgateRoutes'))
+
+const server = app.listen({port, host: "0.0.0.0"}, function(){
+   const host = server.address().address;
+   console.log(`Servidor iniciado`)
+});
