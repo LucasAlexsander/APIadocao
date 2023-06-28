3bf81973d497d800f36b6ce41b1cd8f5f156a890
Lucas Alexsander Barbosa Cruz <lucas10barbosa_@hotmail.com>
2023-06-28 14:00:20 -0300

    Arrumando as funções adicionando o callback

--- src/Model/Adotante.js
+++ src/Model/Adotante.js
@@ -1,31 +1,38 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-class Adotante{
-    //Função para conectar o BD
-    static connect(){
-        //Cria conexao
-        const connection = mysql.createConnection({
-            host:'bd2-ufvjm.mysql.database.azure.com',
-            user:'Mariano',
-            password:'m-88443244',
-            database:'trabbd',
-        });
-        //Conecta ao banco
-        connection.connect();
-        return connection;
-    }
-    static  createAdotante(adotante, callback){
-        const connection = Adotante.connect();
-        const {id_animal, nome_adotante, cpf_adotante, telefone_adotante, endereco_adotante} = adotante;
-        
-       const sql = `INSERT INTO adocao ( id_animal, nome_adotante, cpf_adotante, telefone_adotante, endereco_adotante ) values ( ${id_animal}, '${nome_adotante}', '${cpf_adotante}', '${telefone_adotante}', '${endereco_adotante}')`; 
-        const query = connection.query(sql, function(error, results, fields){
-            if(error) throw error;
-        });
+class Adotante {
+  //Função para conectar o BD
+  static connect() {
+    //Cria conexao
+    const connection = mysql.createConnection({
+      host: "bd2-ufvjm.mysql.database.azure.com",
+      user: "Mariano",
+      password: "m-88443244",
+      database: "trabbd",
+    });
+    //Conecta ao banco
+    connection.connect();
+    return connection;
+  }
+  static createAdotante(adotante, callback) {
+    const connection = Adotante.connect();
+    const {
+      id_animal,
+      nome_adotante,
+      cpf_adotante,
+      telefone_adotante,
+      endereco_adotante,
+    } = adotante;
 
-        console.log(query.sql); 
-        connection.end();
-    }
+    const sql = `INSERT INTO adocao ( id_animal, nome_adotante, cpf_adotante, telefone_adotante, endereco_adotante ) values ( ${id_animal}, '${nome_adotante}', '${cpf_adotante}', '${telefone_adotante}', '${endereco_adotante}')`;
+    const query = connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+    });
+
+    console.log(query.sql);
+    connection.end();
+    callback();
+  }
 }
 
 module.exports = Adotante;
--- src/Model/Resgate.js
+++ src/Model/Resgate.js
@@ -1,31 +1,33 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-class Resgate{
-    //Função para conectar o BD
-    static connect(){
-        //Cria conexao
-        const connection = mysql.createConnection({
-            host:'bd2-ufvjm.mysql.database.azure.com',
-            user:'Mariano',
-            password:'m-88443244',
-            database:'trabbd',
-        });
-        //Conecta ao banco
-        connection.connect();
-        return connection;
-    }
-    static createResgate(resgate, callback){
-        const connection = Resgate.connect();
-        const {id_animal, nome_resgatante, data_resgate, situacao_resgate} = resgate;
-        
-       const sql = `INSERT INTO resgateAnimal ( id_animal, nome_resgatante, data_resgate, situacao_resgate) values ( ${id_animal}, '${nome_resgatante}', '${data_resgate}', '${situacao_resgate}')`; 
-        const query = connection.query(sql, function(error, results, fields){
-            if(error) throw error;
-        });
+class Resgate {
+  //Função para conectar o BD
+  static connect() {
+    //Cria conexao
+    const connection = mysql.createConnection({
+      host: "bd2-ufvjm.mysql.database.azure.com",
+      user: "Mariano",
+      password: "m-88443244",
+      database: "trabbd",
+    });
+    //Conecta ao banco
+    connection.connect();
+    return connection;
+  }
+  static createResgate(resgate, callback) {
+    const connection = Resgate.connect();
+    const { id_animal, nome_resgatante, data_resgate, situacao_resgate } =
+      resgate;
 
-        console.log(query.sql); 
-        connection.end();
-    }
+    const sql = `INSERT INTO resgateAnimal ( id_animal, nome_resgatante, data_resgate, situacao_resgate) values ( ${id_animal}, '${nome_resgatante}', '${data_resgate}', '${situacao_resgate}')`;
+    const query = connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+    });
+
+    console.log(query.sql);
+    connection.end();
+    callback();
+  }
 }
 
 module.exports = Resgate;
