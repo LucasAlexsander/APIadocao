de9738d3642a2dcff1575f338f2c59ca1069acc1
Lucas Alexsander Barbosa Cruz <lucas10barbosa_@hotmail.com>
2023-06-28 11:29:36 -0300

    alterando o method

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const app = express();
 
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
+app.use(cors);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
