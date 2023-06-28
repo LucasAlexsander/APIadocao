3033a0f4bc0389287e72133bed30192607068ef9
Lucas Alexsander Barbosa Cruz <lucas10barbosa_@hotmail.com>
2023-06-28 11:52:03 -0300

    alterando o method

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
-const cors = require("cors");
 
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -21,6 +20,7 @@ app.use((req, res, next) => {
     "POST, GET, OPTIONS, PATCH, DELETE, PUT"
   );
   res.header("Access-Control-Allow-Credentials", "true");
+  app.use(cors());
   next();
 });
 
