50a5007427373fcb50e8f5e0994ff35fdf89f0fa
Lucas Alexsander Barbosa Cruz <lucas10barbosa_@hotmail.com>
2023-06-28 11:42:58 -0300

    alterando o method

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
-app.use(cors);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "apiadocao.onrender.com");
@@ -15,11 +15,11 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
   );
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
     "POST, GET, OPTIONS, PATCH, DELETE, PUT"
   );
-  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
