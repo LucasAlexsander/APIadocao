8b117474ece7e429646d860d8b524e8cfc508fd6
Lucas Alexsander Barbosa Cruz <lucas10barbosa_@hotmail.com>
2023-06-28 11:15:06 -0300

    alterando o method

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
   );
   res.header(
     "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    "POST,GET,OPTIONS,PATCH,DELETE,PUT"
   );
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
